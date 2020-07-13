6a8899d927f1b259edce50ef7f5447598b5a6db8
armoz14 <38024713+armorasha@users.noreply.github.com>
2020-07-13 16:27:49 +0930

    first commit

---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ *  Developer: Raja Palanivel. Copyright: 2019 
+ */
+
+/**
+ * @author 000930322
+ */
+// TODO: check include path
+ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.dirname(__FILE__).'/../../../../php/pear/PHPUnit');
+
+// put your code here
+
+?>
